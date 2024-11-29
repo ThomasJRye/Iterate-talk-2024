9c1cee629831b5a28ca3e3c38da497c3b1395f96
Thomas Rye <thomasjrye@gmail.com>
2024-11-29 17:00:30 +0100

    more wpork

--- loss_util.py
+++ loss_util.py
@@ -2,9 +2,7 @@ import numpy as np
 
 def mean_squared_error(act: np.ndarray, pred: np.ndarray) -> float:
     diff = pred - act
-    print("difference", diff)
     differences_squared = diff ** 2
-    print("difference suqared", differences_squared)
     mean_diff = differences_squared.mean()
     
     return mean_diff
